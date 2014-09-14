0c7079f1a8ecf10fb6a3baf642ced51318080422
Mangirdas Skripka <mangirdas@impresspages.org>
2014-09-14 12:10:19 +0300

    check system version

---
+++ Setup/Worker.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package   ImpressPages
+ */
+
+
+
+
+namespace Plugin\SubscriptionButton\Setup;
+
+
+class Worker
+{
+    public function activate()
+    {
+
+        $version = \Ip\Application::getVersion();
+        $parts = explode('.', $version);
+        if (empty($parts[1]) || $parts[0] < 4 || $parts[1] < 2 ) {
+            throw new \Ip\Exception('ImpressPages 4.2.0 or later required');
+
+        }
+
+
+    }
+}
--- SiteController.php
+++ SiteController.php
@@ -26,6 +26,11 @@ class SiteController extends \Ip\Controller
             'amount' => $widgetData['price'] * 100,
             'currency' => $widgetData['currency']
         );
+
+        if (!empty($widgetData['successUrl'])) {
+            $options['successUrl'] = $widgetData['successUrl'];
+        }
+
         $subscriptionUrl = ipEcommerce()->subscriptionPaymentUrl($options);
 
         return new \Ip\Response\Redirect($subscriptionUrl);
